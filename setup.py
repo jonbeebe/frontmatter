ff0651c940c7da869b21eb8910a0b879e74e21af
Jonathan Beebe <jon@fingerprintplay.com>
2017-12-24 14:19:51 -0800

    Added VERSION txt and updated setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,11 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+with open(path.join(here, 'VERSION'), encoding='utf-8') as f:
+    package_version = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -33,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.2',  # Required
+    version=package_version,  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
