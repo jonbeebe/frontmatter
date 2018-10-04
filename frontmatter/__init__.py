dca2dd8c82afb7465c926edceb8cc95565d231f2
Jonathan Beebe <jonbeebe@users.noreply.github.com>
2018-10-04 01:54:11 -0700

    Removed unnecessary private method and more cleanup.

--- frontmatter/__init__.py
+++ frontmatter/__init__.py
@@ -2,20 +2,14 @@ import re
 import yaml
 
 class Frontmatter:
-    # class properties for internal use that do all the heavy lifting
     _yaml_delim = "^(?:---|\+\+\+)(?:\s+)?\n"
-    _capture_all = "(.+)"
-    _re_pattern = _yaml_delim + _capture_all + _yaml_delim + _capture_all
+    _re_pattern = (_yaml_delim + "(.+)") * 2
     _regex = re.compile(_re_pattern, re.S | re.M)
 
     @classmethod
     def read_file(cls, path):
         """Reads file at path and returns dict with separated frontmatter.
-
-        Returned dict keys:
-        attributes -- extracted YAML attributes in Dictionary form.
-        body -- string contents below the YAML separators
-        frontmatter -- string representation of YAML
+        See read() for more info on dict return value.
         """
         with open(path, encoding="utf-8") as file:
             file_contents = file.read()
@@ -23,29 +17,21 @@ class Frontmatter:
 
     @classmethod
     def read(cls, string):
-        """Reads string and returns dict with separated frontmatter.
+        """Returns dict with separated frontmatter from string.
 
         Returned dict keys:
-        attributes: extracted YAML attributes in Dictionary form.
-        body: str contents below the YAML separators
-        frontmatter: str representation of YAML
+        attributes -- extracted YAML attributes in dict form.
+        body -- string contents below the YAML separators
+        frontmatter -- string representation of YAML
         """
-        parts = cls.__separate_yaml_content(string)
-        fmatter = parts[0]
-        body = parts[1]
-        attributes = yaml.load(fmatter)
-        return {
-            "attributes": attributes,
-            "body": body,
-            "frontmatter": fmatter,
-        }
-
-    @classmethod
-    def __separate_yaml_content(cls, yaml_content=""):
-        """Separates YAML lines from list of strings"""
-        result = cls._regex.search(yaml_content)
+        fmatter = ""
+        body = ""
+        result = cls._regex.search(string)
         if result:
             fmatter = result.group(1)
             body = result.group(2)
-            return (fmatter, body)
-        return ("", "")
+        return {
+            "attributes": yaml.load(fmatter),
+            "body": body,
+            "frontmatter": fmatter,
+        }
