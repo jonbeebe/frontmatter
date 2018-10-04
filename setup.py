69c43da671d34996077b6cd9501d5b057fdc0646
Jonathan Beebe <jonbeebe@users.noreply.github.com>
2018-10-04 02:02:13 -0700

    Updated description in setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='YAML Front Matter parser',  # Required
+    description='Extract YAML frontmatter from files and strings.',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
